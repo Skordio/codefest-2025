44030e380a76a36ebffc6467aae7815afd1e9b6e
Skordio <skordion@hotmail.com>
2025-03-01 12:17:41 -0500

    add vuetify, bootstrap and openapi generator to frontend package

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,24 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(vuetify)
 
 app.use(createPinia())
 app.use(router)
