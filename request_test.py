e6698ca3915e5313388a3b357416e5fef62ef860
Skordio <skordion@hotmail.com>
2025-03-01 10:52:10 -0500

    remove steven request test file and add working request to original file

--- request_test.py
+++ request_test.py
@@ -1,4 +1,9 @@
 import requests
 
-response = requests.get("http://api.phila.gov/phila/calendars/v1?format=json")
-print(response.text)
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+r = requests.get("http://developer.phila.gov/api/phila/calendars/v1?format=json", headers=headers)
+print(dir(r))
+print(r.text)
--- request_test_steven.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-headers = {
-    'Content-Type': 'application/json',
-}
-r = requests.get("http://developer.phila.gov/api/phila/calendars/v1?format=json", headers=headers)
-print(dir(r))
-print(r.status_code)
