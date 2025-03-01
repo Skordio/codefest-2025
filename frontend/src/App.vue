cd8d82154fb806887edd9379993c76f1e8b547df
Skordio <skordion@hotmail.com>
2025-03-01 15:22:18 -0500

    implement basic frontend setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,51 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-layout>
+      <v-app-bar class="bg-surface" dark>
+        <v-container fluid class="d-flex justify-center">
+          <h2>{{ title }}</h2>
+        </v-container>
+      </v-app-bar>
+      <!-- <v-navigation-drawer expand-on-hover rail>
+        <v-list>
+          <v-list-item title="Home"></v-list-item>
+          <v-list-item title="About"></v-list-item>
+        </v-list>
+      </v-navigation-drawer> -->
 
-    <div class="wrapper">
-      <hello-world msg="You did it!" />
-
-      <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-      </nav>
-    </div>
-  </header>
-
-  <router-view />
+      <v-main class="bg-background">
+        <v-container fluid class="d-flex align-center h-100">
+          <v-row class="d-flex" justify="center">
+            <v-col cols="4">
+              <v-card class="bg-secondary elevation-8">
+                <v-card-title>Upload</v-card-title>
+                <v-card-text>
+                  <p>
+                    Upload a video or an audio file about something you want to study:
+                  </p>
+                </v-card-text>
+                <v-container fluid class="d-flex">
+                  <v-file-input label="File input" variant="outlined"></v-file-input>
+                </v-container>
+              </v-card>
+            </v-col>
+          </v-row>
+          <!-- <v-row class="d-flex" justify="center">
+            <v-col cols="4">
+              <v-file-input label="File input" variant="solo-filled"></v-file-input>
+            </v-col>
+          </v-row> -->
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const title = ref("Lock In SG")
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
