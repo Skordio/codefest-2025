88d1b3e8b51777bdab436c4da73652ae6384bdc1
Skordio <skordion@hotmail.com>
2025-03-02 09:26:47 -0500

    implement pdf creation on backend; make frontend offer files to user

--- frontend/src/components/fileUpload.vue
+++ frontend/src/components/fileUpload.vue
@@ -3,7 +3,7 @@
       <v-card-title>Upload</v-card-title>
       <v-card-text>
         <p>
-          Upload a video or an audio file about something you want to study:
+          Upload an audio file containing something you want to study:
         </p>
       </v-card-text>
       <v-container fluid class="d-flex flex-column mt-n5">
@@ -15,14 +15,7 @@
         </div>
         <v-btn color="secondary" @click="transcribe()">Upload</v-btn>
 
-        <v-card v-if="showTranscription" class="mt-5 pa-3">
-          <v-progress-circular v-if="loading" indeterminate class="mx-auto"></v-progress-circular>
-          <div class="d-flex ga-2 justify-center" v-else>
-            <v-btn v-if="studyGuideBlob" color="primary" @click="download(studyGuideBlob, 'study_guide')">Download Study Guide</v-btn>
-            <v-btn v-if="quizBlob" color="primary" @click="download(quizBlob, 'quiz')">Download Quiz</v-btn>
-            <v-btn v-if="flashcardsBlob" color="primary" @click="download(flashcardsBlob, 'flashcards')">Download Flashcards</v-btn>
-          </div>
-        </v-card>
+        <v-progress-circular v-if="loading" indeterminate class="mx-auto mt-5"/>
       </v-container>
     </v-card>
 </template>
@@ -32,10 +25,7 @@
 import * as fs from "fs"
 import { ref } from 'vue'
 import api from '@/api'
-import { createStudyGuidePDF, createFlashCardPDF, createQuizPDF } from '@/utils/pdfCreate'
 
-const showTranscription = ref(false);
-const transcription = ref('');
 const loading = ref(false);
 const file = ref<File>();
 const errorMessage = ref('');
@@ -44,47 +34,44 @@ const studyGuide = ref(true);
 const quiz = ref(false);
 const flashcards = ref(false);
 
-const studyGuideBlob = ref<Blob>();
-const quizBlob = ref<Blob>();
-const flashcardsBlob = ref<Blob>();
+const studyMaterialsBlob = ref(null);
 
 const transcribe = async () => {
   console.log(file.value);
   const formData = new FormData();
   formData.append('file', new Blob([file.value], { type: file.value.type }), file.value.name);
 
-  showTranscription.value = true;
   loading.value = true;
 
   try {
     const response = await api.post(`/create?study_guide=${studyGuide.value}&quiz=${quiz.value}&flash_cards=${flashcards.value}`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
-      }
+      },
+      responseType: 'blob'
     });
 
-    console.log(response.data)
+    // Create a Blob from the response data
+    const studyMaterialsBlob = new Blob([response.data], { type: 'application/zip' });
 
+    // Generate a URL and trigger the download
+    const url = window.URL.createObjectURL(studyMaterialsBlob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'study_materials.zip';
+    document.body.appendChild(a);  // Append to body for Firefox support
+    a.click();
+    document.body.removeChild(a);  // Remove after clicking
+    window.URL.revokeObjectURL(url);
 
-    if (studyGuide.value) {
-      studyGuideBlob.value = createStudyGuidePDF(response.data.studyGuide);
-    }
-    if (quiz.value) {
-      quizBlob.value = createQuizPDF(response.data.quiz);
-    }
-    if (flashcards.value) {
-      flashcardsBlob.value = createFlashCardPDF(response.data.flashcards);
-    }
-
-    // transcription.value = response.data.transcript;
   } catch (error) {
     console.error(error);
     errorMessage.value = error.message;
-    showTranscription.value = false;
   } finally {
     loading.value = false;
   }
 
 }
+
 
 </script>
