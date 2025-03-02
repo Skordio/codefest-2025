28d08961d685d5709527e370e5e31cc767c2b62e
Skordio <skordion@hotmail.com>
2025-03-01 21:59:48 -0500

    redo layout changes that got reverted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,26 +15,37 @@
       </v-navigation-drawer> -->
 
       <v-main class="bg-background">
-        <v-container fluid class="d-flex align-center h-100">
-          <v-row class="d-flex" justify="center">
-            <v-col lg="4" md="6" sm="8" cols="8">
-              <file-upload />
+        <v-card color="background" :class="vCardClass">
+          <template #image>
+            <v-img alt="turtle" class="bg-background h-100" :src="getImgSrc(imgForBp)" cover />
+          </template>
+          <!-- <v-row>
+            <v-col>
+              <v-img src="@/assets/turtle_big.png" alt="turtle">
+
+              </v-img>
             </v-col>
+          </v-row> -->
+          <v-row class="d-flex" :class="vRowClass">
+            <v-col lg="6" md="4" cols="2"/>
+            <v-col lg="4" md="6" cols="8"><file-upload /></v-col>
+            <v-col lg="2" md="2" cols="2"/>
           </v-row>
-        </v-container>
+        </v-card>
       </v-main>
     </v-layout>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { getImgSrc } from './utils';
 import { RouterLink, RouterView } from 'vue-router'
 import fileUpload from './components/fileUpload.vue';
+import { useDisplay } from 'vuetify';
+import { onMounted } from "vue";
 
 const title = ref("Lock In SG")
-
-import { onMounted } from "vue";
 
 let audio: HTMLAudioElement | null = null;  // Explicitly typing the audio variable
 const song = "/mii.mp3";  // Your audio file
@@ -51,5 +62,31 @@ onMounted(() => {
   // Force playback after user interaction
   document.addEventListener("click", playAudio, { once: true });
 });
+
+const { mdAndUp } = useDisplay();
+
+const imgForBp = computed(() => {
+  if (mdAndUp.value) {
+    return 'turtle_desktop.png';
+  } else {
+    return 'turtle_mobile.png';
+  }
+})
+
+const vCardClass = computed(() => {
+  if (mdAndUp.value) {
+    return 'd-flex justify-center align-center h-100 w-100';
+  } else {
+    return 'd-flex flex-column justify-center h-100 w-100';
+  }
+})
+
+const vRowClass = computed(() => {
+  if (!mdAndUp.value) {
+    return 'mt-15';
+  } else {
+    return '';
+  }
+})
 
 </script>
